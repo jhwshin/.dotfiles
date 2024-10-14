4329eb04cc3814cdbb78eba966447f777aa868a0
James Shin <hwshinj@gmail.com>
2024-10-14 21:26:42 +1100

    add: zsh, ohmyzsh, plugins, themes

---
+++ common/zsh/.zsh/0-env.zsh
@@ -0,0 +1,14 @@
+# echo "0-ENV IMPORTED!"
+
+# list env
+# 'printenv'
+
+export PATH=$PATH:$HOME/.scripts:$HOME/.scripts-common
+
+export ZSH=$HOME/.zsh/.oh-my-zsh
+
+export TERMINAL=kitty
+export EDITOR=nvim
+export VISUAL=nvim
+export PAGER=bat
+export MANPAGER=bat
---
+++ common/zsh/.zsh/1-ohmyzsh.zsh
@@ -0,0 +1,20 @@
+# echo "1-OHMYZSH IMPORTED!"
+
+ZSH_THEME="powerlevel10k/powerlevel10k"
+
+plugins=(
+  git
+  # -- CUSTOM PLUGINS
+  zsh-fzf-history-search
+  zsh-autosuggestions
+  fast-syntax-highlighting
+  you-should-use
+)
+
+source $ZSH/oh-my-zsh.sh
+
+# ignore history when prefix with space
+setopt HIST_IGNORE_SPACE
+
+# disable auto pushd
+unsetopt autopushd
